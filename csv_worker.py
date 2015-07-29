642a4d03fec131ce09ca77b753a525f399bfdbb6
alive-web <alivelp@gmail.com>
2015-07-29 14:48:13 +0300

    if file exist ad new row else crate new file

--- csv_worker.py
+++ csv_worker.py
@@ -1,18 +1,17 @@
 import csv
-from datetime import datetime
+import os
 
 
 class WorkWithFile():
     def __init__(self):
         pass
 
-    def write_rows(self, rows, doc_updated):
-        with open('names.csv', 'w') as csvfile:
-            fieldnames = ['first_name', 'last_name', 'doc_updated', 'download_date']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+    def write_rows(self, rows, url):
+        mod = 'w'
+        if os.path.exists("names.csv"):
+            mod = 'a'
+        with open('names.csv', mod) as cs_vfile:
+            fieldnames = ['first_name', 'last_name', 'doc_updated', 'download_date', url]
+            writer = csv.DictWriter(cs_vfile, fieldnames=fieldnames)
             writer.writeheader()
-            for row in rows:
-                row['doc_updated'] = doc_updated
-                row['download_date'] = datetime.now()
-                writer.writerow(row)
+            writer.writerows(rows)
--- parser.py
+++ parser.py
@@ -5,41 +5,46 @@ import urllib2
 import re
 
 
-url = 'http://www.regiongavleborg.se/A-O/Vardgivarportalen/Lakemedel/Lakemedelskommitten/Organisation/' \
-      'Lakemedelskommittens-ledamoter/'
+url = 'http://www.regiongavleborg.se/A-O/Vardgivarportalen/Lakemedel/Lakemedelskommitten/Organisation/Lakemedelskommittens-ledamoter/'
 soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml")
 all_tables = soup.findAll('table', {"class": "linjeradOrange"})
 rows = []
 
 
+def split_full_name(full_name):
+    row = {}
+    if len(full_name) == 2:
+        row['first_name'] = full_name[0].encode('utf-8')
+        row['last_name'] = full_name[1].encode('utf-8')
+    if len(full_name) == 1:
+        row['first_name'] = full_name[0].encode('utf-8')
+    if len(full_name) == 3:
+        row['first_name'] = full_name[0].encode('utf-8') + " " + full_name[1].encode('utf-8')
+        row['last_name'] = full_name[2].encode('utf-8')
+    return row
+
+
 def get_date(html):
-    text_and_date = soup.find(text=re.compile('Uppdaterad'))
+    text_and_date = html.find(text=re.compile('Uppdaterad'))
     updated = ""
     if text_and_date:
         updated = re.search('(\d{1,4}-\d{1,2}-\d{1,2})', str(text_and_date)) or ""
         if updated:
             updated = updated.group(0)
             updated = datetime.strptime(updated, "%Y-%m-%d")
-
     return updated
 
 
 def convert_table_rows(tables):
     for table in tables:
-        trTags = table.find_all("tr")
+        tr_tags = table.find_all("tr")
         needed_el = 0
-        for tr in trTags:
+        for tr in tr_tags:
             if needed_el:
                 full_name = tr.contents[needed_el].contents[0].split()
-                row = {}
-                if len(full_name) == 2:
-                    row['first_name'] = full_name[0].encode('utf-8')
-                    row['last_name'] = full_name[1].encode('utf-8')
-                if len(full_name) == 1:
-                    row['first_name'] = full_name[0].encode('utf-8')
-                if len(full_name) == 3:
-                    row['first_name'] = full_name[0].encode('utf-8') + " " + full_name[1].encode('utf-8')
-                    row['last_name'] = full_name[2].encode('utf-8')
+                row = split_full_name(full_name)
+                row['doc_updated'] = get_date(soup)
+                row['download_date'] = datetime.now()
                 rows.append(row)
                 continue
             for i, el in enumerate(tr.contents):
@@ -48,4 +53,4 @@ def convert_table_rows(tables):
                     continue
 
 convert_table_rows(all_tables)
-WorkWithFile().write_rows(rows, get_date(soup))
+WorkWithFile().write_rows(rows, url)
