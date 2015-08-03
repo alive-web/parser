770ed6c46df419c45f35c5bfd2aa1bf43fc5df77
alive-web <alivelp@gmail.com>
2015-08-03 14:24:32 +0300

    add column title

--- csv_worker.py
+++ csv_worker.py
@@ -6,12 +6,13 @@ class WorkWithFile():
     def __init__(self):
         pass
 
-    def write_rows(self, rows, url):
+    def write_rows(self, rows):
         mod = 'w'
         if os.path.exists("names.csv"):
             mod = 'a'
-        with open('names.csv', mod) as cs_vfile:
-            fieldnames = ['first_name', 'last_name', 'doc_updated', 'download_date', 'role', url]
-            writer = csv.DictWriter(cs_vfile, fieldnames=fieldnames)
-            writer.writeheader()
+        with open('names.csv', mod) as csv_file:
+            fieldnames = ['first_name', 'last_name', 'doc_updated', 'download_date', 'role', 'title', 'url']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            if mod == "w":
+                writer.writeheader()
             writer.writerows(rows)
--- parser.py
+++ parser.py
@@ -8,8 +8,22 @@ import re
 
 url = 'http://www.regiongavleborg.se/A-O/Vardgivarportalen/Lakemedel/Lakemedelskommitten/Organisation/Lakemedelskommittens-terapigrupper/'
 soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml")
-all_tables = soup.findAll('table', {"class": "linjeradOrange"})
 rows = []
+
+
+def parse_tables(html):
+    all_tables = []
+    names = html.findAll('th', text=re.compile('Namn'))
+    for name in names:
+        for parent in name.parents:
+            if parent is not None and parent.name == "table":
+                all_tables.append(parent)
+    if all_tables:
+        convert_table_rows(all_tables)
+
+
+# for string in soup.stripped_strings:
+#     print(repr(string))
 
 
 def get_correct_role(role):
@@ -27,6 +41,26 @@ def get_correct_role(role):
     if roles.has_key(role):
         correct_role = roles[role]
     return correct_role
+
+
+def get_correct_title(title):
+    correct_title = ''
+    titles = {
+        'Distriktssköterska': 'district nurse',
+        'Kvalitetssamordnare': 'quality Coordinator',
+        'Medicinskt ansvarig sjuksköterska': 'Head nurse',
+        'Apotekare': 'pharmacist',
+        'Familjeläkare': 'family',
+        'Överläkare': 'physician',
+        'Tandläkare': 'dentist',
+        'MAS': 'MAS',
+        'Sjuksköterska': 'nurse',
+        'Medicinsk rådgivare': 'medical advisor',
+        'Dietist': 'dietitian'
+    }
+    if titles.has_key(title):
+        correct_title = titles[title]
+    return correct_title
 
 
 def split_full_name(full_name):
@@ -58,26 +92,39 @@ def convert_table_rows(tables):
         tr_tags = table.find_all("tr")
         column_name = False
         column_role = False
+        column_title = False
         for tr in tr_tags:
             row = {}
             if column_name:
                 full_name = tr.contents[column_name].contents[0].split()
                 row = split_full_name(full_name)
-                row['doc_updated'] = get_date(soup)
-                row['download_date'] = datetime.now()
             if column_role:
                 role = tr.contents[column_role].text.encode('utf-8')
                 role = re.sub('\n', '', role)
                 row["role"] = get_correct_role(role)
-            if not column_name and not column_role:
+            if column_title:
+                title = ""
+                title_list = tr.contents[column_title].contents
+                if len(title_list) == 1 or len(title_list) == 2:
+                    title = tr.contents[column_title].contents[0].string.encode('utf-8')
+                if len(title_list) == 3:
+                    title = tr.contents[column_title].contents[2].string.encode('utf-8')
+                row['title'] = get_correct_title(title)
+            if not column_name and not column_role and not column_title:
                 for i, el in enumerate(tr.contents):
-                    if hasattr(el, "text") and el.text == "Namn":
-                        column_name = i
-                    if hasattr(el, "text") and el.text == "Funktion":
-                        column_role = i
+                    if hasattr(el, "text"):
+                        if el.text == "Namn":
+                            column_name = i
+                        if el.text == "Funktion":
+                            column_role = i
+                        if el.text == "Titel":
+                            column_title = i
             else:
+                row['doc_updated'] = get_date(soup)
+                row['download_date'] = datetime.now()
+                row['url'] = url
                 rows.append(row)
+    if rows:
+        WorkWithFile().write_rows(rows)
 
-
-convert_table_rows(all_tables)
-WorkWithFile().write_rows(rows, url)
+parse_tables(soup)
