6711e6f6dd81a59ea0b655d029a156cdf5230225
alive-web <alivelp@gmail.com>
2015-08-05 17:30:41 +0300

    two way are work

--- parser.py
+++ parser.py
@@ -6,31 +6,74 @@ import urllib2
 import re
 
 
-def delete_punctuation(text):
-    text = re.sub('\n', '', text)
-    text = re.sub(',|\.,\?,!', '', text)
-    return text
+def create_obj(all_words):
+    person = {}
+    useless = []
+    useless += all_words
+    for word in all_words:
+        if word.lower() == 'vice':
+            i = all_words.index(word)
+            word = ' '.join((word, all_words[i+1]))
+        if word.lower() == 'medicinsk':
+            i = all_words.index(word)
+            if all_words[i+1].lower() == 'ansvarig':
+                word = ' '.join((word, all_words[i+1], all_words[i+2]))
+            elif all_words[i+1].lower() == u'rådgivare':
+                word = ' '.join((word, all_words[i+1]))
+        role = get_correct_role(word.encode('utf-8'))
+        if role:
+            person['role'] = role
+            for el in word.split():
+                useless.remove(el)
+            continue
+        title = get_correct_title(word.encode('utf-8'))
+        if title:
+            person['title'] = title
+            for el in word.split():
+                useless.remove(el)
+            continue
+        if re.match(r"^[a-zA-Z0-9._]+\@[a-zA-Z0-9._]+\.[a-zA-Z]{2,}$", word):
+            person['email'] = word
+            useless.remove(word)
+            continue
+    if len(useless) >= 2:
+        if len(useless[0]) == 2:
+            person['first_name'] = ' '.join((useless[0], useless[1])).encode('utf-8')
+            person['last_name'] = useless[2].encode('utf-8')
+            useless = useless[3:]
+        else:
+            person['first_name'] = useless[0].encode('utf-8')
+            person['last_name'] = useless[1].encode('utf-8')
+            useless = useless[2:]
+    person['url'] = url
+    person['workplace'] = ' '.join(useless).encode('utf-8')
+    person['doc_updated'] = get_date(soup)
+    person['download_date'] = datetime.now()
+    return person
 
 
 def test(html):
-    roles = html.findAll(text=re.compile('l.{1}kare|mas|apotekare', re.I))
+    roles = html.findAll(text=re.compile('l.{1}kare|mas|apotekare|medicinsk|sk.{1}terska|'
+                                         'samordnare|dietist|barnmorska|bakteriolog', re.I))
+    previous_parent = ''
+    rows = []
     for role in roles:
         parent = role.find_parent(['tr', 'li'])
-        if parent:
-            text = parent.text
-            text = delete_punctuation(text)
-            print text
-
-
-def parse_tables(html):
-    all_tables = []
-    names = html.findAll('th', text=re.compile('Namn'))
-    for name in names:
-        for parent in name.parents:
-            if parent is not None and parent.name == "table":
-                all_tables.append(parent)
-    if all_tables:
-        convert_table_rows(all_tables)
+        if parent and parent != previous_parent:
+            all_words = []
+            for string in parent.stripped_strings:
+                free = re.compile("Vakant", re.I)
+                if re.match(free, string):
+                    all_words = []
+                    break
+                string = re.sub('\n|,|\?|!|>|Sammankallande', '', string)
+                all_words += string.split()
+            if all_words:
+                row = create_obj(all_words)
+                rows.append(row)
+        previous_parent = parent
+    if rows:
+        WorkWithFile().write_rows(rows)
 
 
 def get_correct_role(role):
@@ -47,8 +90,10 @@ def get_correct_role(role):
 def get_correct_title(title):
     title = title.lower()
     titles = {
+        'specialistläkare': 'specialist doctors',
+        'st-läkare': 'resident physician',
         'distriktssköterska': 'district nurse',
-        'kvalitetssamordnare': 'quality Coordinator',
+        'kvalitetssamordnare': 'quality coordinator',
         'medicinskt ansvarig sjuksköterska': 'Head nurse',
         'apotekare': 'pharmacist',
         'familjeläkare': 'family',
@@ -58,7 +103,9 @@ def get_correct_title(title):
         'mas': 'MAS',
         'sjuksköterska': 'nurse',
         'medicinsk rådgivare': 'medical advisor',
-        'dietist': 'dietitian'
+        'dietist': 'dietitian',
+        'barnmorska': 'midwife',
+        'bakteriolog': 'bacteriological'
     }
     return titles.get(title, '')
 
@@ -100,8 +147,12 @@ def convert_table_rows(tables):
             if column_name:
                 full_name = tr.contents[column_name].contents[0].split()
                 full_name = split_full_name(full_name)
-                person['first_name'] = full_name[0]
-                person['last_name'] = full_name[1]
+                if len(full_name) == 2:
+                    person['first_name'] = full_name[0]
+                    person['last_name'] = full_name[1]
+                elif len(full_name) == 1:
+                    person['first_name'] = full_name[0]
+                    person['first_name'] = full_name[0]
                 if len(tr.contents[column_name].contents) > 3:
                     for el in tr.contents[column_name].contents:
                         if hasattr(el, "text") and el.text:
@@ -146,8 +197,20 @@ def convert_table_rows(tables):
     if rows:
         WorkWithFile().write_rows(rows)
 
+
+def parse_tables(html):
+    all_tables = []
+    names = html.findAll('th', text=re.compile('Namn'))
+    for name in names:
+        for parent in name.parents:
+            if parent is not None and parent.name == "table":
+                all_tables.append(parent)
+    if all_tables:
+        convert_table_rows(all_tables)
+
+
 if __name__ == "__main__":
-    url = 'http://www.regiongavleborg.se/A-O/Vardgivarportalen/Lakemedel/Lakemedelskommitten/Organisation/Lakemedelskommittens-ledamoter/'
+    url = 'http://www.regiongavleborg.se/A-O/Vardgivarportalen/Lakemedel/Lakemedelskommitten/Organisation/Lakemedelskommittens-terapigrupper/'
     soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml")
     # parse_tables(soup)
     test(soup)
